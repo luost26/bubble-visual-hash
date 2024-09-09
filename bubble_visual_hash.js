f467a3edd9f57c904b794daaf72f340ed75bb9da
Shitong Luo <luost26@gmail.com>
2024-09-09 00:55:58 -0400

    Wrap rendering into a function

--- bubble_visual_hash.js
+++ bubble_visual_hash.js
@@ -102,11 +102,13 @@ drawBubble = function (svg, bubbleInfo) {
     }
 }
 
-var canvases = document.querySelectorAll(".bubble-visual-hash");
-canvases.forEach(function (canvas) {
-    var hash = canvas.getAttribute("data-bubble-visual-hash");
-    var width = canvas.viewBox.baseVal.width;
-    var height = canvas.viewBox.baseVal.height;
-    var bubbleInfo = createBubbleInfo(createHashGroupForString(hash), 8, width, height);
-    drawBubble(canvas, bubbleInfo);
-});
+renderBubbleVisualHash = function () {
+    var canvases = document.querySelectorAll(".bubble-visual-hash");
+    canvases.forEach(function (canvas) {
+        var hash = canvas.getAttribute("data-bubble-visual-hash");
+        var width = canvas.viewBox.baseVal.width;
+        var height = canvas.viewBox.baseVal.height;
+        var bubbleInfo = createBubbleInfo(createHashGroupForString(hash), 8, width, height);
+        drawBubble(canvas, bubbleInfo);
+    });
+}
